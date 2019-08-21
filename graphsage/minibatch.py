f8b99d2fc3c16f4347caf072f2a0a8c1eabac0cf
hust-wuwenchao <2591099581@qq.com>
2019-08-21 21:14:20 +0800

    2019-08-21_21:14:20+提交进行的提交

--- graphsage/minibatch.py
+++ graphsage/minibatch.py
@@ -235,6 +235,7 @@ class NodeMinibatchIterator(object):
     #如何构造进阶矩阵
     def construct_adj(self):
         adj = len(self.id2idx)*np.ones((len(self.id2idx)+1, self.max_degree))
+        print("adj",adj.shape)
         print("len(self.id2idx)",len(self.id2idx))
         deg = np.zeros((len(self.id2idx),))
 
